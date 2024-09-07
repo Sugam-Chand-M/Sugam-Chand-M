39336ef6b6da20b9064eb7a24f64f9117192230d
Sugam Chand M <sugamgowda284d@gmail.com>
2024-09-07 19:50:47 +0530

    Phase 2
    Developed the header tab feature using framer motion, tailwind css. Added the required nav links i.e., the sections in the header tab along with project images

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
-import wordanalyticsImg from "@/public/wordanalytics.png";
+import sociopediaApp from "@/public/sociopedia.png";
+import myMemoryApp from "@/public/myMemoryApp.jpg";
+import { title } from "process";
 
 export const links = [
   {
@@ -16,6 +16,14 @@ export const links = [
     hash: "#about",
   },
   {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Education",
+    hash: "#education",
+  },
+  {
     name: "Projects",
     hash: "#projects",
   },
@@ -23,9 +31,13 @@ export const links = [
     name: "Skills",
     hash: "#skills",
   },
+  // {
+  //   name: "Open Source Contribution",
+  //   hash: "#opensource", 
+  // },
   {
-    name: "Experience",
-    hash: "#experience",
+    name: "Hobbies",
+    hash: "#hobbies",
   },
   {
     name: "Contact",
@@ -33,75 +45,79 @@ export const links = [
   },
 ] as const;
 
+export const education = [
+  {
+    title: "B.E/B.Tech",
+    college: "Bangalore Instituite of Technology",
+    major:"Computer Science and Engineering",
+    cgpa: "9.44/10.0",
+    icon: React.createElement(LuGraduationCap),
+    date: "August 2019 - May 2023",
+  }
+];
+
 export const experiencesData = [
   {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
+    title: "Software Engineer Intern",
+    location: "Bangalore, India",
     description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2019",
-  },
-  {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
-    description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+      "",
     icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
+    date: "February 2023 - August 2023",
   },
   {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
+    title: "Associate Software Engineer",
+    location: "Bangalore, India",
     description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
+      "",
+    icon: React.createElement(CgWorkAlt),
+    date: "September 2023 - Present",
   },
+  // {
+  //   title: "Full-Stack Developer",
+  //   location: "Houston, TX",
+  //   description:
+  //     "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+  //   icon: React.createElement(FaReact),
+  //   date: "2021 - present",
+  // },
 ] as const;
 
 export const projectsData = [
   {
-    title: "CorpComment",
+    title: "Sociopedia React Application",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+      "",
+    tags: ["React","JavaScript", "MongoDB", "Tailwind", "Prisma"],
+    imageUrl: sociopediaApp,
   },
   {
-    title: "rmtDev",
+    title: "My Memory Application",
     description:
-      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
-    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
-    imageUrl: rmtdevImg,
-  },
-  {
-    title: "Word Analytics",
-    description:
-      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
-    imageUrl: wordanalyticsImg,
+      "",
+    tags: ["Kotlin", "App Development"],
+    imageUrl: myMemoryApp,
   },
 ] as const;
 
+// export const openSourceData=[
+
+// ];
+
 export const skillsData = [
-  "HTML",
-  "CSS",
+  "C++",
+  "Java",
   "JavaScript",
   "TypeScript",
   "React",
   "Next.js",
   "Node.js",
   "Git",
+  "GitHub",
+  "GitLab",
   "Tailwind",
   "Prisma",
   "MongoDB",
-  "Redux",
-  "GraphQL",
-  "Apollo",
-  "Express",
   "PostgreSQL",
-  "Python",
-  "Django",
   "Framer Motion",
 ] as const;
